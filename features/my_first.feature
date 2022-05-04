0853c53420a36ec68f954537da73c5b15ca34d80
Dario Fernando Rojas <df.rojasr1@uniandes.edu.co>
2022-05-03 21:05:46 -0500

    Archivo de configuracion y prueba de funcionamiento

--- features/my_first.feature
+++ features/my_first.feature
@@ -1,7 +1,12 @@
-Feature: Primera prueba
+Feature: Iniciar una conversación
 
-@user1 @web
-Scenario:Mi escenario 1
-  Given I navigate to page "http://localhost:2368/"
- Given I wait for a signal containing "hi"
- Then I wait 10 seconds
+  @user1 @web
+  Scenario: Como primer usuario inicio sesion y mandó un mensaje al usuario 2
+    Given I navigate to page "https://www.messenger.com/login"
+    And I wait for 5 seconds
+    When I enter email "<USERNAME1>"
+    And I wait for 2 seconds
+    And I enter password "<PASSWORD1>"
+    And I wait for 2 seconds
+    And I click next
+    And I wait for 4 seconds
--- features/web/step_definitions/step.js
+++ features/web/step_definitions/step.js
@@ -1,1 +1,24 @@
-const { Given, When, Then } = require('@cucumber/cucumber');
+const { Given, When, Then } = require("@cucumber/cucumber");
+
+Given("I have {int} cukes in my belly", function (cukes) {
+  console.log(`Cukes: ${cukes}`);
+});
+
+When("I enter email {kraken-string}", async function (email) {
+  console.log("Ingresar email: ",email);
+  let element = await this.driver.$("#email");
+
+  return await element.setValue(email);
+});
+
+When("I enter password {kraken-string}", async function (password) {
+  let element = await this.driver.$("#pass");
+
+  return await element.setValue(password);
+});
+
+When("I click next", async function () {
+  let element = await this.driver.$("#loginbutton");
+
+  return await element.click();
+});
